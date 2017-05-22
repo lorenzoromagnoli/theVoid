3560d72a224019f488c2fe314e032fa9d2728e9d
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-05-22 13:09:48 +0200

    provided right link to game

--- theVoidBot_serverCode/index.js
+++ theVoidBot_serverCode/index.js
@@ -6,7 +6,7 @@ const token = '386674205:AAHVM5SohpgQF8vrhf1050tnnbtMkmpc4Cc';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-var gameLink=""
+var gameLink="https://lorenzoromagnoli.github.io/theVoid/";
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -36,7 +36,7 @@ bot.on('message', (msg) => {
   bot.sendMessage(chatId, 't.me/theVoid_bot?game=theVoid');
 });
 
-bot.on("callback_query", function(msg) {
-  bot.answerCallbackQuery(msg.id, gameLink, true, {url: gameLink});
+bot.on("callback_query", function(callbackQuery) {
+  bot.answerCallbackQuery(callbackQuery.id, gameLink, true, {url: gameLink});
   console.log(callbackQuery);
 });
